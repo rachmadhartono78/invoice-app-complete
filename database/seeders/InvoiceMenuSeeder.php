f8200a6880986f7b5b53457d53829befdc0acd6f
rachmadhartono78 <rahmadhartono78@gmail.com>
2026-02-12 00:33:47 +0700

    feat: add Invoice menu structure seeder

--- database/seeders/InvoiceMenuSeeder.php
+++ database/seeders/InvoiceMenuSeeder.php
@@ -59,6 +59,12 @@ class InvoiceMenuSeeder extends Seeder
                 'icon' => '<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/></svg>',
                 'order' => 5,
             ],
+            [
+                'name' => 'Cetak Invoice',
+                'url' => 'invoices',
+                'icon' => '<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"/></svg>',
+                'order' => 6,
+            ],
         ];
 
         foreach ($childMenus as $menuData) {
